d0e5adc32eb8a99b8e8569c5a0b891e257c13db2
Z-Shang <shangzhanlin@gmail.com>
2014-09-12 04:17:19 -0500

    add toggle screen to bright

--- bright.lua
+++ bright.lua
@@ -1,12 +1,18 @@
 local wibox = require("wibox")
 local awful = require("awful")
 
-b_widget = wibox.widget.textbox()
+local bright = {}
+
+local b_widget = wibox.widget.textbox()
 b_widget:set_align("right")
 
-function update_brightness(widget)
+local screen_b = 0
+local screen_on = true
+
+local function update_brightness(widget)
     local b = io.popen("xbacklight")
     local status = b:read("*all")
+    screen_b = string.format("%d", status)
     b:close()
 
     local brightness = string.format("%d", status) .. "%"
@@ -14,8 +20,29 @@ function update_brightness(widget)
     widget:set_markup(brightness)
 end
 
+
+local function toggle_screen()
+        local b = io.popen("xbacklight")
+        local status = b:read("*all")
+        screen_b = string.format("%d", status)
+        b:close()
+    if tonumber(screen_b) > 0 then
+        os.execute("xbacklight -set 0")
+        return false
+    else
+        os.execute("xbacklight -inc 100")
+        return true
+    end
+    return false
+end
+
 update_brightness(b_widget)
 
 mytimer = timer({ timeout = 0.2 })
 mytimer:connect_signal("timeout", function () update_brightness(b_widget) end)
 mytimer:start()
+
+bright.b_widget = b_widget
+bright.toggle_screen = toggle_screen
+
+return bright
